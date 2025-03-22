4274d7a1a744aa812a436d58c60edde542102ead
Nixixouwu <ni.aliaga@duocuc.cl>
2025-03-22 17:42:55 -0300

    e

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject('API_URL') private apiUrl: string
+  ) { }
 
   // Helper method to get auth headers
   private getHeaders(): HttpHeaders {
